fcb7799a7dfd22b1e1681151594e5b2f902dbb86
Heil Park <heilpark@me.com>
2023-03-25 14:21:37 +0000

    fix link

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -7,7 +7,7 @@ function Home() {
       <div className="home mx-auto text-center">
         <div className=""><h1>Welcome to my Portfolio!</h1></div>
         <p>My name is Heil Park!</p>
-        <button><a href="about">View my portfolio!</a></button>
+        <button><a href="#/about">View my portfolio!</a></button>
         <div><strong>OR</strong></div>
         <button>
           <a href="../../CV.pdf" download="CV.pdf">Download my CV!</a>
