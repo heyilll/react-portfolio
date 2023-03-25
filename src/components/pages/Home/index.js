011ee8bc616af5e2ca238c1af7bd2e468ef925c5
Heil Park <heilpark@me.com>
2023-03-25 14:27:16 +0000

    chaned button

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -6,9 +6,7 @@ function Home() {
     <div className="bg ">
       <div className="home mx-auto text-center">
         <div className=""><h1>Welcome to my Portfolio!</h1></div>
-        <p>My name is Heil Park!</p>
-        <button><a href="#/about">View my portfolio!</a></button>
-        <div><strong>OR</strong></div>
+        <p>My name is Heil Park!</p> 
         <button>
           <a href="../../CV.pdf" download="CV.pdf">Download my CV!</a>
         </button>
